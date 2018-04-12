b457f647f02cad0c2d442fc50bc1a1accc11197f
LuckyXixi <410421448@qq.com>
2018-04-13 01:04:14 +0800

    Revert "button error"
    This reverts commit d31693ca1a61896487c7ec71d3ea4b6ae9e05916.

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'hello word',
+    motto: 'Hello World',
     userInfo: {},
     hasUserInfo:false ,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
